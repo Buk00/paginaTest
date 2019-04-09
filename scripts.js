f30a76c3a9c98ce2af51af9238a8f36b4f29f462
Buk00 <tortorella00@gmail.com>
2019-04-09 14:48:08 +0200

    mods

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 
+const k = 10;
+let x = 0;
+let fullList = []
+
+const updateList = () =>{
+  const content = document.getElementById('content')
+  content.innerHTML = fullList.slice(x*k, k*(x+1)).reduce((acc, e) => {
+  acc += `<article><h2><span> ${e.Title} </span></h2><p> ${e.Description} </p> </article>`
+  return acc
+}, "")
+}
+
+
+fetch("https://fakerestapi.azurewebsites.net/api/Books")
+.then(res => res.json())
+.then(data => {
+  fullList = data
+  updateList()
+})
+.catch(err=> {
+  console.error(err)
+})
+const next = () => {
+  x++
+  content.innerHTML = `<img src="https://media1.tenor.com/images/7e77e0a227b7270cf65601afd5e39339/tenor.gif?itemid=12901256" class="cr7" alt="Milan Juventus Ronaldo GIF - MilanJuventus Ronaldo Happy GIFs"/>`
+  setTimeout( function () {updateList()}, 3200)
+}
