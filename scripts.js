3f3a1b829c8f529a7c9aee586c36282c399b5654
Buk00 <tortorella00@gmail.com>
2019-04-09 14:19:44 +0200

    changed paginator scripts

--- scripts.js
+++ scripts.js
@@ -1,29 +1,32 @@
-const content = document.getElementById('content')
-const pagination = document.getElementById('pagination');
-const itemForPage = 10;
+const k = 10;
+let x = 0;
+let fullList = []
 
-fetch('https://fakerestapi.azurewebsites.net/api/Books')
-  .then(res => res.json())
-  .then(data => {
-    const htmlPages = data.reduce((acc, {Title, Description}, index) => {
-      if(index % itemForPage === 0) {
-        acc.push([])
-      }
-      acc[acc.length - 1].push(`<article><h2><span> ${Title} </span></h2><p> ${Description} </p></article>`);
-      return acc;
-    }, []);
-    console.log(htmlPages)
-    content.innerHTML = htmlPages.shift().join('');
-    pagination.onclick = () => {
-			if(htmlPages.length > 0) {
-				content.innerHTML = `<img src="https://media1.tenor.com/images/7e77e0a227b7270cf65601afd5e39339/tenor.gif?itemid=12901256" class="cr7" alt="Milan Juventus Ronaldo GIF - MilanJuventus Ronaldo Happy GIFs"/>`
-      	setTimeout(function () {
-      		content.innerHTML = htmlPages.shift().join('')
-				}, 3200)       
-    	}
-  	}
+const updateList = () =>{
+  const content = document.getElementById('content')
+  content.innerHTML = fullList.slice(x*k, k*(x+1)).reduce((acc, e) => {
+  acc += `<article><h2><span> ${e.Title} </span></h2><p> ${e.Description} </p> </article>`
+  return acc
+}, "")
+}
+
+
+fetch("https://fakerestapi.azurewebsites.net/api/Books")
+.then(res => res.json())
+.then(data => {
+  fullList = data
+  updateList()
 })
- .catch(console.error);
+.catch(err=> {
+  console.error(err)
+})
+const next = () => {
+  x++
+  content.innerHTML = `<img src="https://media1.tenor.com/images/7e77e0a227b7270cf65601afd5e39339/tenor.gif?itemid=12901256" class="cr7" alt="Milan Juventus Ronaldo GIF - MilanJuventus Ronaldo Happy GIFs"/>`
+  setTimeout( function () {updateList()}, 3200)
+}
+
+
 
 
 
